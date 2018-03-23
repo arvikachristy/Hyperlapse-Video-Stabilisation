e58d4d1178e1f591a649ce18af10f1e6145459c9
arvikachristy <anastasia.arvika@gmail.com>
2018-03-23 22:46:53 +0000

    split ransac to function and fix some dumb typo :deciduous_tree:

--- MAIN.m
+++ MAIN.m
@@ -6,14 +6,12 @@ inputImage = imresize(inputImage, 0.3);
 
 cm_storage = [];
 
-for x = 1:imageN-1
-    fprintf('%i\n',x);
-    holder_model = [];
-    holder_perc = 0;
-    hodler_diff = 0;
+for i = 1:imageN-1
+%     fprintf('%i\n',x);
+
     %Find matching points
-    img1 = rgb2gray(inputImage(:,:,:,x));
-    img2 = rgb2gray(inputImage(:,:,:,x+1));
+    img1 = rgb2gray(inputImage(:,:,:,i));
+    img2 = rgb2gray(inputImage(:,:,:,i+1));
     
     points1 = detectHarrisFeatures(img1);
     points2 = detectHarrisFeatures(img2);
@@ -27,41 +25,11 @@ for x = 1:imageN-1
     matchedPoints2 = valid_points2(indexPairs(:,2),:);
     
 %     figure; showMatchedFeatures(img1,img2,matchedPoints1,matchedPoints2);
-
-    for ran = 1:200
-
-        index_st = randperm(size(matchedPoints1,1),4);
-
-        pnt1 = matchedPoints1(index_st, :).Location;
-        pnt2 = matchedPoints2(index_st, :).Location;
-        
-        try
-            model = fitgeotrans(pnt1, pnt2, 'projective');
-        catch
-            continue;
-        end 
-        
-        [x,y] = transformPointsForward(model, matchedPoints1.Location(:,1),...
-            matchedPoints1.Location(:,2)); 
-
-        distx = (x - matchedPoints2.Location(:,1)).^2;
-        disty = (y - matchedPoints2.Location(:,2)).^2;
-
-        diff = distx + disty;
-
-        threshold = 1;
-        percentage = mean(diff<1); %PErcentage of inliers
-
-        if(percentage >holder_perc)
-            holder_perc = percentage;
-            holder_model = model;
-            holder_diff = diff;
-        end
-    end
-
+    [holder_diff, holder_model] = ransac(matchedPoints1, matchedPoints2);
+    
     cost1 = mean(holder_diff);
 
-    [x,y] = transformPointsForward(model, width/2, height/2); 
+    [x,y] = transformPointsForward(holder_model, width/2, height/2); 
 
     distx = (x - width/2).^2;
     disty = (y - height/2).^2;
@@ -73,11 +41,11 @@ for x = 1:imageN-1
     tc = 0.1*d;
     g = 0.5*d;
 
-    cm = 0;
     if(cost1<tc)
         cm = cost2; 
     else
         cm = g;  
     end
+    cm_storage = [cm_storage, cm];
 end
 implay(inputImage);
